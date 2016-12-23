3b1bc3ac618d3d28d04828c1f39099797b0d6213
Evan Jarrett <ejfirestar00@gmail.com>
2016-12-22 20:45:01 -0800

    updates

--- sorter.js
+++ sorter.js
@@ -1,28 +1,41 @@
-var Unsorted = videos;
-var Total = Unsorted.length;
-var Sorted = [];
-var Ignored = [];
-var Round = 1;
-var GroupSize = [];
-var newGroupSize = [];
-var GroupA = [];
-var GroupB = [];
-var GroupC = [];
-var Alpha = {};
-var Beta = {};
-var End = false;
-var alpha_selector = $("#alpha-btn");
-var beta_selector = $("#beta-btn");
+var unsorted = videos;
+var sorted = [];
+var ignored = [];
+var round = 0;
+var group_size = [];
+var new_group_size = [];
+var group_a = [];
+var group_b = [];
+var group_c = [];
+var alpha = {};
+var beta = {};
+var end = false;
+var alpha_video_btn = document.getElementById("alpha-btn");
+var beta_video_btn = document.getElementById("beta-btn");
+
+var alpha_ignore_btn = document.getElementById("alpha-ignore");
+var beta_ignore_btn = document.getElementById("beta-ignore");
 
 function isIgnored(x) {
-    return $.inArray(x, Ignored);
+    return ignored.indexOf(x.name);
 }
-$("#ignorebtn").click(function () {
-    Ignored.push(Alpha);
-    Ignored.push(Beta);
-    console.log("Ignoring " + Alpha.name + " and " + Beta.name);
-    alpha_selector.trigger("click");
-});
+
+function ignoreSong(song, choose) {
+    ignored.push(song.name);
+    console.log("Ignoring " + song.name);
+    choose.click();
+}
+
+function ignoreA() {
+    ignoreSong(alpha, beta_video_btn);
+}
+
+function ignoreB() {
+    ignoreSong(beta, alpha_video_btn);
+}
+
+alpha_ignore_btn.addEventListener("click", ignoreA);
+beta_ignore_btn.addEventListener("click", ignoreB);
 
 // Loads the IFrame Player API code asynchronously.
 var tag = document.createElement('script');
@@ -32,6 +45,7 @@ firstScriptTag.parentNode.insertBefore(tag, firstScriptTag);
 
 var alpha_player, beta_player;
 var alpha_vid_id, beta_vid_id;
+var youtube_is_loaded = false;
 function onYouTubeIframeAPIReady() {
     // There is probably a race condition here.
     // This gets loaded asynchronously, but if it hits this before the IDs are set we might have a problem
@@ -47,169 +61,173 @@ function onYouTubeIframeAPIReady() {
             videoId: beta_vid_id
         });
     }
+    youtube_is_loaded = true;
 }
 
 function loadPicks() {
     //Setup groups
-    if (GroupA.length + GroupB.length == 0) {
+    if (group_a.length + group_b.length == 0) {
         var x, y;
-        y = GroupSize.shift();
-        while (GroupA.length < y && Unsorted.length !== 0) {
-            x = Unsorted.shift();
+        y = group_size.shift();
+        while (group_a.length < y && unsorted.length !== 0) {
+            x = unsorted.shift();
             if (x == undefined) break;
-            console.log("Added " + x.name + " to GroupA");
-            GroupA.push(x);
+            console.log("Added " + x.name + " to group_a");
+            group_a.push(x);
         }
-        y = GroupSize.shift();
-        while (GroupB.length < y && Unsorted.length !== 0) {
-            x = Unsorted.shift();
+        y = group_size.shift();
+        while (group_b.length < y && unsorted.length !== 0) {
+            x = unsorted.shift();
             if (x == undefined) break;
-            console.log("Added " + x.name + " to GroupB");
-            GroupB.push(x);
+            console.log("Added " + x.name + " to group_b");
+            group_b.push(x);
         }
     }
 
     //Load picks
-    Alpha = GroupA.shift();
-    Beta = GroupB.shift();
+    alpha = group_a.shift();
+    beta = group_b.shift();
 
-    document.getElementById("alpha-btn").innerHTML = Alpha.name;
-    document.getElementById("beta-btn").innerHTML = Beta.name;
-    alpha_vid_id = Alpha.vidId;
-    beta_vid_id = Beta.vidId;
-    // So this is working because race conditions
-    // First time through the player object hasn't been created
-    // But we set a global vid_id that will load the video onReady
-    alpha_player.loadVideoById(alpha_vid_id);
-    alpha_player.stopVideo();
-    beta_player.loadVideoById(beta_vid_id);
-    beta_player.stopVideo();
-
-    Round++;
-    document.getElementById("round").innerHTML = "Round: " + Round;
-    console.log("ROUND " + Round + "!\n" + Alpha.name + " VS " + Beta.name);
-
-    if ((isIgnored(Alpha)) != -1) {
-        Ignored.push(Alpha);
-        console.log(Alpha.name + " is ignored, " + Beta.name + " wins automatically.");
-        beta_selector.trigger("click");
+    if (isIgnored(alpha) != -1) {
+        ignored.push(alpha.name);
+        console.log("Auto Win " + beta.name);
+        console.log("Ignoring " + alpha.name);
+        run("beta-btn");
+        return;
     }
-    else if ((isIgnored(Beta)) != -1) {
-        Ignored.push(Beta);
-        console.log(Beta.name + " is ignored, " + Alpha.name + " wins automatically.");
-        alpha_selector.trigger("click");
+    else if (isIgnored(beta) != -1) {
+        ignored.push(beta.name);
+        console.log("Auto Win " + alpha.name);
+        console.log("Ignoring " + beta.name);
+        run("alpha-btn");
+        return;
+    }
+
+    round++;
+    document.getElementById("round").innerHTML = "Round: " + round;
+    console.log("ROUND " + round + "!\n" + alpha.name + " VS " + beta.name);
+
+    alpha_video_btn.innerHTML = alpha.name;
+    beta_video_btn.innerHTML = beta.name;
+    alpha_vid_id = alpha.vidId;
+    beta_vid_id = beta.vidId;
+    if (youtube_is_loaded) {
+        alpha_player.loadVideoById(alpha_vid_id);
+        alpha_player.stopVideo();
+        beta_player.loadVideoById(beta_vid_id);
+        beta_player.stopVideo();
     }
 }
 
 function init() {
     var i;
-    for (i = 0; i < Unsorted.length; i++) {
-        GroupSize.push(1);
-    }
-    for (i = 0; i < Total - 1; i++) { //Randomize ship order
-        Sorted.push(Unsorted.splice(Math.floor(Math.random() * Unsorted.length), 1)[0]);
+    alpha_video_btn.addEventListener('click', run);
+    beta_video_btn.addEventListener('click', run);
+    for (i = 0; i < unsorted.length; i++) {
+        group_size.push(1);
     }
 
-    Sorted.push(Unsorted[0]);
-    Unsorted = Sorted;
-    Sorted = [];
-    console.log(Unsorted);
+    console.log(unsorted);
     loadPicks();
 }
 
-
-$(".video_btn").click(function (event) {
-    console.log("Unsorted : " + Unsorted.length);
-    console.log("Sorted: " + Sorted.length);
-    event.preventDefault();
-    if (End == true) {
+function run(button) {
+    console.log("unsorted : ");
+    console.log(unsorted);
+    console.log("sorted: ");
+    console.log(sorted);
+    if (end == true) {
         return;
     }
     //Deal with the winner
-    if (event.target.id == "alpha-btn") {
-        console.log(Alpha.name + " wins!");
-        if (Alpha == undefined) return;
-        GroupC.push(Alpha);
-        GroupB.unshift(Beta);
+    if ((this.getAttribute && this.getAttribute("id") == "alpha-btn") || button == "alpha-btn") {
+        console.log(alpha.name + " wins!");
+        if (alpha == undefined) return;
+        group_c.push(alpha);
+        group_b.unshift(beta);
     }
-    else {
-        console.log(Beta.name + " wins!");
-        if (Beta == undefined) return;
-        GroupC.push(Beta);
-        GroupA.unshift(Alpha);
+    else if ((this.getAttribute && this.getAttribute("id") == "beta-btn") || button == "beta-btn") {
+        console.log(beta.name + " wins!");
+        if (beta == undefined) return;
+        group_c.push(beta);
+        group_a.unshift(alpha);
     }
-    //If either group empty, add all to GroupC
-    if (GroupA.length == 0 || GroupB.length == 0) {
-        while (GroupA.length != 0) {
-            x = GroupA.shift();
-            GroupC.push(x);
+    //If either group empty, add all to group_c
+    if (group_a.length == 0 || group_b.length == 0) {
+        while (group_a.length != 0) {
+            x = group_a.shift();
+            group_c.push(x);
         }
-        while (GroupB.length != 0) {
-            x = GroupB.shift();
-            GroupC.push(x);
+        while (group_b.length != 0) {
+            x = group_b.shift();
+            group_c.push(x);
         }
-        if (GroupSize.length == 1) { //If only one group remaining, bonus round with GroupC
-            console.log("Extra Round!");
-            while (GroupC.length != 0) {
-                x = GroupC.shift();
+        if (group_size.length == 1) { //If only one group remaining, bonus round with group_c
+            console.log("Extra round!");
+            while (group_c.length != 0) {
+                x = group_c.shift();
                 if (x == undefined) break;
-                console.log("Added " + x.name + " to GroupA");
-                GroupA.push(x);
+                console.log("Added " + x.name + " to group_a");
+                group_a.push(x);
             }
-            y = GroupSize.shift();
-            while (GroupB.length < y) {
-                x = Unsorted.shift();
+            y = group_size.shift();
+            while (group_b.length < y) {
+                x = unsorted.shift();
                 if (x == undefined) break;
-                console.log("Added " + x.name + " to GroupB");
-                GroupB.push(x);
+                console.log("Added " + x.name + " to group_b");
+                group_b.push(x);
             }
         }
         else {
-            //Add GroupC to sorted
-            newGroupSize.push(GroupC.length); //Get the new size of the group
-            while (GroupC.length != 0) {
-                x = GroupC.shift();
-                Sorted.push(x);
+            //Add group_c to sorted
+            new_group_size.push(group_c.length); //Get the new size of the group
+            while (group_c.length != 0) {
+                x = group_c.shift();
+                sorted.push(x);
             }
         }
     }
 
-    //if all Unsorted sorted, add all ships back to Sorting pool
-    if (Unsorted.length == 0 && GroupA.length + GroupB.length == 0) {
-        while (Unsorted != 0) Sorted.push(Unsorted.pop);
-        while (Sorted.length != 0) {
-            x = Sorted.shift();
-            console.log(x + " added to Unsorted");
-            Unsorted.push(x);
+    //if all unsorted sorted, add all them back to Sorting pool
+    if (unsorted.length + group_a.length + group_b.length == 0) {
+        while (sorted.length != 0) {
+            x = sorted.shift();
+            console.log(x.name + " added to unsorted");
+            unsorted.push(x);
         }
-        while (newGroupSize.length != 0) {
-            GroupSize.push(newGroupSize.shift());
+        while (new_group_size.length != 0) {
+            group_size.push(new_group_size.shift());
         }
-        //End
-        if (GroupSize.length + newGroupSize.length == 1) {
-            $("#ignorebtn").hide(); //hide ignore button
-            Unsorted = Unsorted.filter(function (x) {
-                return Ignored.indexOf(x) < 0
-            }); //remove ignored ships
-            console.log(Unsorted);
-            End = true;
-            var Winner = Unsorted.shift();
+        //end
+        if (group_size.length + new_group_size.length == 1) {
+            document.getElementById("vid-group").style.display = "none";
 
-            document.getElementById("round").innerHTML = "Round: " + Round + "/" + Round;
+            unsorted = unsorted.filter(function (x) {
+                return ignored.indexOf(x) < 0
+            }); //remove ignored
+            console.log(unsorted);
+            end = true;
+            var Winner = unsorted.shift();
+
+            document.getElementById("round").innerHTML = "round: " + round + "/" + round;
             document.getElementById("subtitle").innerHTML = "All sorted!<br>" + Winner.name + " wins";
-            while (Unsorted.length != 0) {
-                var x = Unsorted.shift();
-                var y = $(document.createElement('p'));
-                y.html(x.name);
-                y.appendTo('#losers');
+            while (unsorted.length != 0) {
+                var x = unsorted.shift();
+                var li = document.createElement('li');
+                li.className = "mdl-list__item";
+                var span = document.createElement('span');
+                span.className = "mdl-list__item-primary-content";
+                span.innerHTML = x.name;
+
+                li.appendChild(span);
+                document.getElementById("losers").appendChild(li);
+                document.getElementById("final-list").style.display = "";
             }
             return;
         }
     }
-
     loadPicks();
-
-});
+}
 
 //Start
 init();
